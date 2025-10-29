473b7164a355bca2c44965e03c41878b6154b6d6
Fabian Distler <fabian.distler@gmx.net>
2025-10-29 13:01:59 +0100

    Add data model

---
+++ src/volume_data.py
@@ -0,0 +1,8 @@
+"""Muscle volume landmarks for chest muscle group."""
+
+volume_landmarks = {
+    "muscle_group": "chest",
+    "beginner": {"MEV": 8, "MAV": 12, "MRV": 16},
+    "intermediate": {"MEV": 10, "MAV": 15, "MRV": 20},
+    "advanced": {"MEV": 12, "MAV": 18, "MRV": 24},
+}
